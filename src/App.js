41a346475af1a387c08efe2c0dc3c136ca7ef068
aardvarkpepper <aardvarkpepper@gmail.com>
2023-03-09 18:17:48 -0500

    Move home files into home folder, created various files, bit of code added in App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 
 // const test = process.env.REACT_APP_YOUTUBE_API_KEY;
@@ -6,7 +7,14 @@ function App() {
   // console.log(test);
   return (
     <div className="App">
-
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/videos/:topic" element={<Videos />} />
+          <Route path="/video/:id" element={<Video />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
