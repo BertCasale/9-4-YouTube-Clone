678c46c0a9cb96e6ff68c2ac4ea4abe96aa8bf77
aardvarkpepper <aardvarkpepper@gmail.com>
2023-03-10 04:23:09 -0500

    Add css styling to 'About'.

--- src/Components/about/Developer.js
+++ src/Components/about/Developer.js
@@ -1,10 +1,14 @@
-export default function Developer({developerBio}) {
+export default function Developer({ developerBio }) {
     return (
-        <div>
-            <img className="developerPicture" src={developerBio.picture} alt={`An image of ${developerBio.name.firstname} ${developerBio.name.lastname}}`} />
-            <h3>{developerBio.name.firstname} {developerBio.name.lastname}</h3>
-            <p>{developerBio.bio}</p>
-            <p>{developerBio.github}</p>
+        <div className="developerCard">
+            <div className="portraitContainer">
+                <img className="developerPortrait" src={developerBio.picture} alt={`An image of ${developerBio.name.firstname} ${developerBio.name.lastname}}`} />
+            </div>
+            <div className="textContainer">
+                <h2>{developerBio.name.firstname} {developerBio.name.lastname}</h2>
+                <p>{developerBio.bio}</p>
+                <p>{developerBio.github}</p>
+            </div>
         </div>
     );
 }
