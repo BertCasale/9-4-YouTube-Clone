8d4ad4d56cd9589788fdf56b401ab49683318277
BertCasale <bthebest55@gmail.com>
2023-03-10 20:55:10 -0500

    get the search results and show on videos component

--- src/Components/videos/Videos.js
+++ src/Components/videos/Videos.js
@@ -1,3 +1,39 @@
+import { useParams, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getTopic } from "../../Api/fetch";
+import "./Videos.css"
+
 export default function Videos() {
-  return(<div></div>);
+  const { topic } = useParams();
+  let [searchResults, setSearchResults] = useState([]);
+
+  //fetch the search results once when the page renders
+  useEffect(() => {
+    getTopic(topic)
+    .then((response) => {
+      setSearchResults([...response.items]);
+    })
+  }, []);
+
+
+  return(<div className="search-results">
+    {/* {console.log(searchResults)} */}
+    {searchResults.map((video) => {
+      return(<div 
+        className="video-card"
+        key={video.id.videoId}>
+        
+        <Link to="/video/:id">
+          <img
+            src={video.snippet.thumbnails.high.url}
+            alt={video.snippet.title}
+            height="150px"
+          />
+          <h3>{video.snippet.title}</h3>
+          <h4>{video.snippet.channelTitle}</h4>
+        </Link>
+
+      </div>);
+    })}
+  </div>);
 }
