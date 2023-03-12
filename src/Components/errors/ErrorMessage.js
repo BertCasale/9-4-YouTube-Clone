d8702586a936d40021c2dde89e59f6a07bbd35ce
BertCasale <bthebest55@gmail.com>
2023-03-12 11:40:18 -0400

    create error message with basic css

--- src/Components/errors/ErrorMessage.js
+++ src/Components/errors/ErrorMessage.js
@@ -1,3 +1,11 @@
-export default function error() {
-  return(<div></div>);
+import "./ErrorMessage.css"
+
+export default function ErrorMessage() {
+  return(<div className="error-modal">
+    <div className="modal-message">
+      <button className="close-button">x</button>
+    <h2>There was an issue with your request. Please try again.</h2>
+    </div>
+    
+  </div>);
 }
--- src/Components/home/Home.js
+++ src/Components/home/Home.js
@@ -1,3 +1,9 @@
+//remove this
+//import "../errors/ErrorMessage"
+import ErrorMessage from "../errors/ErrorMessage";
 export default function Home() {
-  return(<div></div>);
+  return(<div>
+    {/* remove this too, for showing error message before code is made */}
+    {/* <ErrorMessage/> */}
+  </div>);
 }
