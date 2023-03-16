2b5886cc548876a34dfb34a7d786d5582bf00d45
BertCasale <bthebest55@gmail.com>
2023-03-15 21:15:50 -0400

    filter variables and fetch changes to use them

--- src/Components/common/Navbar.js
+++ src/Components/common/Navbar.js
@@ -3,20 +3,34 @@ import { useState } from "react";
 import "./Navbar.css"
 
 export default function Navbar() {
-  const [search, setSearch] = useState("");
   const [showFilters, setShowFilters] = useState(false);
   const navigate = useNavigate();
-
-  //when the text changes, set the search state
+  let search = "";
+  let sortingBy = "relevance";
+  let quantity = "20";
+  let safeSearch = "moderate";
+  
   function textChange(event) {
-    setSearch(event.target.value);
+    search = event.target.value;
+  }
+
+  function quantityChange(event) {
+    quantity = event.target.value;
+  }
+
+  function sortChange(event) {
+    sortingBy = event.target.value;
+  }
+
+  function safeSearchChange(event) {
+    safeSearch = event.target.value;
   }
 
   //when the search form is submitted, navigate to the page with the videos
   function searchSubmit(event) {
     event.preventDefault();
     event.target.searchbox.value = "";
-    navigate(`/videos/${encodeURIComponent(search)}`);
+    navigate(`/videos/${encodeURIComponent(search)}&maxResults=${quantity}&order=${sortingBy}&safeSearch=${safeSearch}`);
   }
 
   function showOrHideFilters(event) {
@@ -51,18 +65,19 @@ export default function Navbar() {
           max="50"
           id="quantity"
           name="quantity"
-          defaultValue="20"/></p>
+          defaultValue="20"
+          onChange={quantityChange}/></p>
 
         <p><label htmlFor="sortby">Sort By:</label>
-        <select id="sortby" name="sortby" defaultValue="Relevance">
-          <option value="Relevance">Relevance</option>
-          <option value="Date">Date</option>
-          <option value="Rating">Rating</option>
-          <option value="Title">Title</option>
+        <select id="sortby" name="sortby" defaultValue="relevance" onChange={sortChange}>
+          <option value="relevance">Relevance</option>
+          <option value="date">Date</option>
+          <option value="rating">Rating</option>
+          <option value="title">Title</option>
         </select></p>
 
         <p><label htmlFor="safe-search">Safe Search:</label>
-        <select id="safe-search" name="safe-search" defaultValue="Moderate">
+        <select id="safe-search" name="safe-search" defaultValue="moderate" onChange={safeSearchChange}>
           <option value="moderate">Moderate</option>
           <option value="none">None</option>
           <option value="strict">Strict</option>
